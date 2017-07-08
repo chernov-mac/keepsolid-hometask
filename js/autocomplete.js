8cfa8d76a7d2de3e47ebb276739437fa6408d461
Vladimir Chernov <chernov.mac@gmail.com>
2017-07-09 00:12:25 +0300

    Refactored class Autocomplete, layout and styles

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,151 +1,197 @@
 /*
-    JavaScript autoComplete
-    Author: Vladimir Chernov
-    For KeepSolid Summer Internship 2017
+*    JavaScript autoComplete
+*    Author: Vladimir Chernov
+*    For KeepSolid Summer Internship 2017
 */
+/*jslint esversion: 6 */
+
+const defaultOptions = {
+    noSuggestionText: 'No matches',
+    arrowNavigation: true,
+    symbolsToStart: 1, // start matching from this symbols amount
+    limit: 5, // 0 - all suggestions
+    sort: 'ASC', // ASC || DESC
+    onSelect: null, // callback function on item select
+    errorClass: 'error', // sort order
+    highliteMatchesClass: 'highlite' // span.highlite
+};
 
 class Autocomplete {
 
-    constructor(options) {
+    constructor(input, data, options) {
 
-        // options
-        this.limit = options.limit || 5;
-        this.symbolsToStart = options.symbolsToStart || 1;
+        this.input = input;
+        this.data = data;
+        this.options = Object.assign({}, defaultOptions, options); // merge options
+        this.resultData = [];
 
-        // wrap input in customizable boxes
-        this.input = document.querySelector(options.element);
-        // div.form-control to contain input itself
-        this.fc = document.createElement('div');
-        this.fc.classList.add('form-control');
-        // div.autocomplete to contain .form-group, .dialog and buttons
-        this.ac = document.createElement('div');
-        this.ac.classList.add('autocomplete');
-        this.ac.appendChild(this.fc);
-        this.input.parentNode.insertBefore(this.ac, this.input);
-        let origin = this.input.cloneNode(true);
-        this.input.parentNode.removeChild(this.input);
-        this.fc.appendChild(origin);
-        this.input = origin;
+        this.init();
+    }
 
-        // create close button
-        this.close = document.createElement('span');
-        this.close.classList.add('close');
-        this.ac.appendChild(this.close);
+    get isOpened() {
+        return this._isOpened;
+    }
 
-        // create dialog to contain spacer and body with options
-        let spacer = document.createElement('div');
-        spacer.classList.add('spacer');
-        let dialogBody = document.createElement('div');
-        dialogBody.classList.add('body');
-        this.dialog = document.createElement('div');
-        this.dialog.classList.add('dialog');
-        this.dialog.appendChild(spacer);
-        this.dialog.appendChild(dialogBody);
-        this.ac.appendChild(this.dialog);
-        this.dialog = dialogBody;
+    set isOpened(value) {
+        this._isOpened = value;
+        if(value) {
+            this.ac.classList.add('active');
+        } else {
+            this.ac.classList.remove('active');
+        }
+    }
 
-        // load data and init work
-        if(options.data) {
-            this.data = Object.values(options.data).sort();
+    init() {
+        this.wrapElements();
+        this.createButtons();
+        this.createDialog();
+        this.setData();
+        this.initHandlers();
+    }
+
+    setData() {
+        if(typeof this.data != 'string') {
+            this.data = Object.values(this.data);
+            this.sortData();
+            this.resultData = this.data;
             this.setDialogList();
-        } else if(options.dataURL) {
-            this.getDataByURL(options.dataURL);
+        } else if(typeof this.data == 'string') {
+            this.getDataByURL(this.data);
         } else {
             console.error('There is no data to autocomplete.');
             return false;
         }
+    }
 
-        // HANDLERS
+    wrapElements() {
+        // div.autocomplete to contain .form-group, .dialog and buttons
+        this.ac = document.createElement('div');
+        this.ac.classList.add('autocomplete');
 
-        this.initHandlers();
+        // div.form-control to contain input itself
+        this.fc = this.input.closest('.form-control');
+        if (!this.fc) {
+            this.fc = document.createElement('div');
+            this.fc.classList.add('form-control');
+            this.ac.appendChild(this.fc);
+            this.input.parentNode.insertBefore(this.ac, this.input);
+            let originInput = this.input.cloneNode(true);
+            this.input.parentNode.removeChild(this.input);
+            this.fc.appendChild(originInput);
+            this.input = originInput;
+        } else {
+            this.fc.parentNode.insertBefore(this.ac, this.fc);
+            let originFC = this.fc.cloneNode(true);
+            let originInput = this.input.cloneNode(true);
+            this.fc.parentNode.removeChild(this.fc);
+            this.ac.appendChild(originFC);
+            this.fc = originFC;
+            this.input = originInput;
+            this.fc.innerHTML = '';
+            this.fc.appendChild(this.input);
+        }
+    }
 
+    createButtons() {
+        // create close button
+        this.close = document.createElement('span');
+        this.close.classList.add('close');
+        this.ac.appendChild(this.close);
+    }
+
+    createDialog() {
+        // create dialog to contain spacer and suggestions block with options
+        let spacer = document.createElement('div');
+        spacer.classList.add('spacer');
+        let suggestions = document.createElement('ul');
+        suggestions.classList.add('suggestions');
+        this.dialog = document.createElement('div');
+        this.dialog.classList.add('dialog');
+        this.dialog.appendChild(spacer);
+        this.dialog.appendChild(suggestions);
+        this.ac.appendChild(this.dialog);
+        this.suggestions = suggestions;
+        this.isOpened = false;
     }
 
     getDataByURL(url) {
-        var _ = this;
-
         fetch(url)
             .then(function(response) {
                 return response.json();
             })
             .then(function(result){
-                _.data = Object.values(result).sort();
-                _.setDialogList();
+                this.data = result;
+                this.sortData();
+                this.resultData = this.data;
+                this.setDialogList();
             })
             .catch(console.log);
     }
 
     clearDialog() {
-        this.dialog.innerHTML = '';
+        this.suggestions.innerHTML = '';
         this.fc.classList.remove('error');
     }
 
     setDialogList() {
         this.clearDialog();
-        for (let i = 0; i < this.data.length; i++) {
-            let elem = document.createElement('div');
-            let text = document.createTextNode(this.data[i]);
-            elem.appendChild(text);
-            elem.classList.add('option');
-            elem.setAttribute('tabindex', i+1);
-            elem.setAttribute('data-val', this.data[i]);
-            this.dialog.appendChild(elem);
+
+        let items = '';
+        if (!this.resultData.length) {
+            this.fc.classList.add(this.options.errorClass);
+            items = `<li class="${this.options.errorClass}">${this.options.noSuggestionText}</li>`;
+        } else {
+            const hl = new RegExp(`<span.*?>(.+?)</span>`, 'gi');
+
+            for (let i = 0; i < this.resultData.length; i++) {
+                let value = this.resultData[i];
+
+                let dataValue = value.replace(hl, '$1');
+
+                items += `<li class="option" tabindex="${i + 1}" data-val="${dataValue}">${value}</li>`;
+            }
+        }
+
+        this.suggestions.innerHTML = items;
+    }
+
+    sortData() {
+        switch (this.options.sort) {
+            case 'ASC':
+                this.data.sort();
+                break;
+            case 'DESC':
+                this.data.sort().reverse();
+                break;
         }
     }
 
     match(str) {
-        str = str.toLowerCase();
-        this.fc.classList.remove('error');
-        this.clearDialog();
+        this.resultData = [];
 
-        var count = 0;
-        for (let i = 0; i < this.data.length; i++) {
-            let row = '';
+        const value = this.input.value;
+        const regexp = new RegExp(`(${value})`, 'gi');
 
-            let pos = 0;
-            while (true) {
-                let foundPos = this.data[i].toLowerCase().indexOf(str, pos);
+        let count = 0;
+        let limit = this.options.limit || this.data.length;
 
-                if (foundPos == -1) {
-                    if (pos === 0) break;
-                    else {
-                        row += this.data[i].substr(pos);
-
-                        let elem = document.createElement('div');
-                        elem.innerHTML = row;
-                        elem.classList.add('option');
-                        elem.setAttribute('tabindex', count+1);
-                        elem.setAttribute('data-val', this.data[i]);
-                        this.dialog.appendChild(elem);
-                        count++;
-
-                        break;
-                    }
-                } else {
-                    row += this.data[i].slice(pos, foundPos) +
-                        '<span class="highlite">' +
-                            this.data[i].substr(foundPos, str.length) +
-                        '</span>';
-                    pos = foundPos + str.length;
-                }
+        for(let i = 0; i < this.data.length && count < limit; i++) {
+            let item = this.data[i];
+            const matches = item.match(regexp);
+            if (matches) {
+                const string = item.replace(regexp, `<span class="${this.options.highliteMatchesClass}">$1</span>`);
+                this.resultData[this.resultData.length] = string;
+                count++;
             }
-            if (count >= this.limit) break;
         }
-        if (count === 0) {
-            this.fc.classList.add('error');
-
-            let elem = document.createElement('div');
-            elem.classList.add('error');
-            elem.innerHTML = 'No matches';
-            this.dialog.appendChild(elem);
-        }
+        this.setDialogList();
     }
 
     selectOption(str) {
-        alert('You chose '+str);
         this.input.value = str;
+        str && this.options.onSelect && this.options.onSelect.call(this, str);
+        this.match(str);
         this.input.focus();
-        this.dialog.classList.remove('open');
         this.close.classList.add('visible');
     }
 
@@ -158,29 +204,29 @@ class Autocomplete {
     }
 
     onInputFocus() {
-        this.ac.classList.add('active');
-        this.dialog.classList.add('open');
+        this.isOpened = true;
     }
 
     onInput() {
         this.close.classList.add('visible');
-        if (this.value.length >= this.symbolsToStart) {
-            if(e.keyCode != 38 && e.keyCode != 40) {
-                this.match(this.value);
+        if (this.input.value.length >= this.options.symbolsToStart) {
+            if(event.keyCode != 38 && event.keyCode != 40) {
+                this.match(this.input.value);
             }
         } else {
             this.close.classList.remove('visible');
+            this.resultData = this.data;
             this.setDialogList();
         }
     }
 
     onClose() {
+        this.resultData = this.data;
         this.setDialogList();
 
         this.close.classList.remove('visible');
-        this.ac.classList.add('active');
-        this.dialog.classList.add('open');
-
+        // this.ac.classList.add('active');
+        this.isOpened = true;
 
         this.input.value = '';
         this.input.focus();
@@ -189,16 +235,16 @@ class Autocomplete {
     onKeyPress(event) {
         switch (event.keyCode) {
             case 38: // if the UP key is pressed
-                if (document.activeElement == this.input || document.activeElement == this.dialog.firstChild) { this.input.focus(); }
-                else if (this.dialog.contains(document.activeElement)) { document.activeElement.previousSibling.focus(); }
+                if (document.activeElement == this.input || document.activeElement == this.suggestions.firstChild) { this.input.focus(); }
+                else if (this.suggestions.contains(document.activeElement)) { document.activeElement.previousSibling.focus(); }
                 break;
             case 40: // if the DOWN key is pressed
-                if (document.activeElement == this.input) { this.dialog.firstChild.focus(); }
-                else if (document.activeElement == this.dialog.lastChild) { break; }
-                else if (this.dialog.contains(document.activeElement)) { document.activeElement.nextSibling.focus(); }
+                if (document.activeElement == this.input) { this.suggestions.firstChild.focus(); }
+                else if (document.activeElement == this.suggestions.lastChild) { break; }
+                else if (this.suggestions.contains(document.activeElement)) { document.activeElement.nextSibling.focus(); }
                 break;
             case 13: // if the ENTER key is pressed
-                if (this.dialog.contains(document.activeElement)) {
+                if (this.suggestions.contains(document.activeElement)) {
                     this.selectOption(document.activeElement.getAttribute('data-val'));
                 }
                 break;
@@ -206,11 +252,11 @@ class Autocomplete {
     }
 
     onOuterClick(event) {
-        if (event.target !== this.input) {
-            this.ac.classList.remove('active');
+        if (event.target !== this.input && event.target !== this.close && !this.suggestions.contains(event.target)) {
+            this.isOpened = false;
         }
 
-        if (event.target.classList.contains('option') && this.dialog.contains(document.activeElement)) {
+        if (this.suggestions.contains(document.activeElement)) {
             this.selectOption(event.target.getAttribute('data-val'));
         }
     }
